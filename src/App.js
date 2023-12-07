ce6ee3dc3366f6d96f99962ab42138c9dd1335fa
Heber <parzival.html@gmail.com>
2023-12-07 18:50:51 +0100

    Install and configure React folder structure.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Blog, Features, Footer, Header, Possibility, Whatgpt3 } from './containers';
+import { Article, Brand, CTA, Navbar } from './components';
+
+const App = () =>{
+    return(
+        <div>
+            <h1>OpenAI Landing page</h1>
+        </div> 
+    )
+}
+
+export default App
---
+++ src/components/cta/CTA.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './cta.css';
+
+const CTA = () =>{
+    return(
+        <div>
+            <h1>CTA component</h1>
+        </div>
+    )
+}
+
+export default CTA
